702b37f86422056b22f2c3c6dabce16a228e19f4
Nicolas Schallock <nicolasschallock@Nicolass-MacBook-Pro-5.local>
2024-05-01 19:54:37 -0700

    fix

--- signup.js
+++ signup.js
@@ -5,9 +5,10 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
-app.post('/recoverPassword', (req, res) => {
+app.post('/signup', (req, res) => {
     //res.send(`email is:${req.body.email}.`);
-    res.redirect('/project.html')
+    console.log(req.body.email)
+    res.redirect('/home.html')
     
   });
   
